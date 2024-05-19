36dfa256aecf16d1608e278e3a4068168cf1a77b
Iwobi Okwudili Frank <iwobiokwudili@gmail.com>
2024-05-19 21:26:26 +0100

    Moved default screens up a level

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HamburgerIcon from './components/icons/HamburgerIcon.vue';
 </script>
 
 <template>
-    <main class="container mx-auto py-16 px-6 mb:px-8 max-w-7xl min-w-sm" >
+    <!-- <main class="container mx-auto py-16 px-6 mb:px-8 max-w-7xl min-w-sm" > -->
         <!-- Nav Section -->
         <nav class="container flex justify-between items-center">
             <div>
@@ -29,11 +29,11 @@ import HamburgerIcon from './components/icons/HamburgerIcon.vue';
         <!-- Hero Section -->
         <section class="container mt-16 flex flex-col-reverse items-center md:flex-row md:justify-between md:mt-32 md:items-start" >
             <div class="flex flex-col space-y-6 items-center md:items-baseline" >
-                <h1 class="font-bold sm:w-md max-w-lg text-6xl text-secondary text-center leading-[60px] md:text-left">
+                <h1 class="font-bold text-4xl xs:w-sm sm:w-md max-w-lg sm:text-6xl text-secondary text-center sm:leading-[60px] md:text-left">
                     Bring everyone together
                     to build better products.
                 </h1>
-                <p class="text-2xl sm:w-md max-w-md text-center text-dark-grey md:text-left md:px-0" >
+                <p class="text-lg sm:text-2xl xs:w-sm sm:w-md max-w-md text-center text-dark-grey md:text-left md:px-0" >
                     Manage makes it simple for software teams to
                     plan day-to-day tasks while keeping the larger team
                     goals in view.
@@ -48,5 +48,5 @@ import HamburgerIcon from './components/icons/HamburgerIcon.vue';
             </div>
         </section>
         <!-- End Hero Section -->
-    </main>
+    <!-- </main> -->
 </template>
